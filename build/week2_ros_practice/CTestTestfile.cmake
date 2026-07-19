9c4cf128a8fd44bfa9e7ddde46a9faafe670927c
Chrarath <2834244344@qq.com>
2026-07-19 23:58:02 +0800

    上传代码

---
+++ build/week2_ros_practice/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wh/catkin_ws/src/week2_ros_practice
+# Build directory: /home/wh/catkin_ws/build/week2_ros_practice
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
